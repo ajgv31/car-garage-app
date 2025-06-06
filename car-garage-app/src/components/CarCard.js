5650cd4589161f643d38d08c7730ca5372dbd1a8
Aj Veloso <ajveloso21@gmail.com>
2025-06-06 16:55:26 -0400

    Update CarCard.js

--- car-garage-app/src/components/CarCard.js
+++ car-garage-app/src/components/CarCard.js
@@ -1,35 +1,34 @@
 import React from "react";
 import carImage from "../images/derix.jpg";
 
-const CarCard = ({ cars, deleteCarHandler, setCurrentCar }) => 
-    {
-        const { id, make, model, colour, year, plateNumber, fault } = props.car;
-            return 
-            (
-                <div className="item">
-                <img className="ui avatar image" src={carImage} alt="car" />
-                <div className="content">
-                    <div className="header">
+const CarCard = ({ car, deleteCarHandler, editHandler }) => {
+    const { id, make, model, colour, year, plateNumber, fault } = car;
+    
+    return (
+        <div className="item">
+            <img className="ui avatar image" src={carImage} alt="car" />
+            <div className="content">
+                <div className="header">
                     {make} {model} ({year})
-                    </div>
-                    <div>
+                </div>
+                <div>
                     <p>Colour: {colour}</p>
                     <p>Plate: {plateNumber}</p>
                     <p>Fault: {fault}</p>
-                    </div>
                 </div>
-                <i
-                    className="edit alternate outline icon"
-                    style={{ color: "blue", marginTop: "7px", marginRight: "10px" }}
-                    onClick={() => props.editHandler(id)}
-                ></i>
-                <i
-                    className="trash alternate outline icon"
-                    style={{ color: "red", marginTop: "7px" }}
-                    onClick={() => props.clickHandler(id)}
-                ></i>
-                </div>
-            );
+            </div>
+            <i
+                className="edit alternate outline icon"
+                style={{ color: "blue", marginTop: "7px", marginRight: "10px" }}
+                onClick={() => editHandler(id)}
+            ></i>
+            <i
+                className="trash alternate outline icon"
+                style={{ color: "red", marginTop: "7px" }}
+                onClick={() => deleteCarHandler(id)}
+            ></i>
+        </div>
+    );
 };
 
 export default CarCard;
