2cd85654d9a82a74198d5742248c8b69fb7e105f
Aj Veloso <ajveloso21@gmail.com>
2025-06-06 20:07:52 -0400

    clean

--- car-garage-app/src/components/CarList.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const CarList = () => 
-{
-
-};
-
-export default CarList;
--- car-garage-app/src/components/MechanicCard.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const MechanicCard = (props) =>
-{
-
-};
-
-export default MechanicCard;
