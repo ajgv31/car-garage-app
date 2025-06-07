60ad604d4e53c700de609d666ef01f267117918a
Aj Veloso <ajveloso21@gmail.com>
2025-06-06 20:11:44 -0400

    delete redundant components

--- car-garage-app/src/components/AddMechanic.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-class AddMechanic extends React.Component 
-{
-
-};
-
-export default AddMechanic;
--- car-garage-app/src/components/MechanicList.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import MechanicCard from "./MechanicCard";
-
-const MechanicList = (props) => 
-{
-
-};
-
-export default MechanicList;
